9d24e1614b20a570823b5720f6ac64a1dc3ec8ec
SelimEminoglu61 <56717476+SelimEminoglu61@users.noreply.github.com>
2026-02-06 15:40:34 +0300

    Update UI With BaseUI
    Add BaseUI library and put first element

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,76 @@
-import "./styleLoginPage.css";
+import { useState } from "react";
+import { Field } from "@base-ui/react/field";
+import { Form } from "@base-ui/react/form";
+import { Button } from "@base-ui/react/button";
+
+import styles from "./styleLoginPage.module.css";
 
 function LoginPage() {
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
   return (
-    <>
-      <div className="container">
-        <div className="loginDiv">
-          <h1>Login</h1>
-          <form>
-            <label>
-              Username:
-              <input type="text" name="username" />
-            </label>
-            <label>
-              Password:
-              <input type="password" name="password" />
-            </label>
-            <button type="submit">Login</button>
-          </form>
-        </div>
-      </div>
-    </>
+    <div className="container">
+      <h1>Selim's Weather App</h1>
+      <Form
+        className={styles.Form}
+        errors={errors}
+        onSubmit={async (event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const value = String(formData.get("url"));
+
+          setLoading(true);
+          const response = await submitForm(value);
+          const serverErrors = {
+            url: response.error,
+          };
+
+          setErrors(serverErrors);
+          setLoading(false);
+        }}
+      >
+        <Field.Root name="url" className={styles.Field}>
+          <Field.Label className={styles.Label}>Homepage</Field.Label>
+          <Field.Control
+            type="url"
+            required
+            defaultValue="https://example.com"
+            placeholder="https://example.com"
+            pattern="https?://.*"
+            className={styles.Input}
+          />
+          <Field.Error className={styles.Error} />
+        </Field.Root>
+        <Button
+          type="submit"
+          disabled={loading}
+          focusableWhenDisabled
+          className={styles.Button}
+        >
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 }
 
+async function submitForm(value) {
+  // Mimic a server response
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+
+  try {
+    const url = new URL(value);
+
+    if (url.hostname.endsWith("example.com")) {
+      return { error: "The example domain is not allowed" };
+    }
+  } catch {
+    return { error: "This is not a valid URL" };
+  }
+
+  return { success: true };
+}
 export default LoginPage;
